b000e2555484f617796bfbf749ffa3f6ae7193a5
hugofficial <41154353+hugofficial@users.noreply.github.com>
2020-03-25 20:54:46 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -6,13 +6,6 @@ const connection = new Sequelize("guiapress", "root", "root", {
   timezone: "-03:00",
   logging: false
 });
-/*
-const connection = new Sequelize("guiapresshbg", "oniloguh2609", "-5d-2NQTxf", {
-  host: "mysql669.umbler.com",
-  dialect: "mysql",
-  timezone: "-03:00",
-  logging: false
-});
-*/
+
 module.exports = connection;
 //0900_ai
